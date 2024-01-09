c45d03e4fd6828e9d1e2f94761e9bfd223eb17d0
KimDongEon <gold3987@naver.com>
2024-01-09 18:26:05 +0900

    first push

---
+++ cshelper/src/main/java/com/cshelper/cshelper/CshelperApplication.java
@@ -0,0 +1,13 @@
+package com.cshelper.cshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CshelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CshelperApplication.class, args);
+	}
+
+}
---
+++ cshelper/src/test/java/com/cshelper/cshelper/CshelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cshelper.cshelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CshelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
