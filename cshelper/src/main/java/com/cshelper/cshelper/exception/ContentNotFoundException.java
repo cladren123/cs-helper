94d3f441dc2348b6d0397fed3eac96dd5ab8c86c
KimDongEon <gold3987@naver.com>
2024-01-15 03:02:25 +0900

    feat(ContentNotFoundException) : id가 DB에 없을 때 발생하는 예외 생성

---
+++ cshelper/src/main/java/com/cshelper/cshelper/exception/ContentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cshelper.cshelper.exception;
+
+
+
+public class ContentNotFoundException extends RuntimeException {
+  public ContentNotFoundException(String message) {
+    super(message);
+  }
+}
