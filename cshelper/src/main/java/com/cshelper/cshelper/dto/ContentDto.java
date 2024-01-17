be06e0e64d5b02af322e9cb39eff0bd777f3a7e8
KimDongEon <gold3987@naver.com>
2024-01-18 00:29:11 +0900

    feat(ContentDto) : 카테고리 이름 추가

--- cshelper/src/main/java/com/cshelper/cshelper/dto/ContentDto.java
+++ cshelper/src/main/java/com/cshelper/cshelper/dto/ContentDto.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class ContentDto {
 
   private String category;
+  private String categoryName;
   private String title;
   private String content;
   private Integer level;
