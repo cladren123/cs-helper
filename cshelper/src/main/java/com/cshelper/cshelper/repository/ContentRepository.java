b3e8e17a7d4b90d01fa4cde1dc3ca500ee789b4f
KimDongEon <gold3987@naver.com>
2024-01-18 00:45:34 +0900

    feat(ContentRepository) : ContentDto 조회 메소드 생성

--- cshelper/src/main/java/com/cshelper/cshelper/repository/ContentRepository.java
+++ cshelper/src/main/java/com/cshelper/cshelper/repository/ContentRepository.java
@@ -1,10 +1,21 @@
 package com.cshelper.cshelper.repository;
 
 
+import com.cshelper.cshelper.dto.ContentDto;
 import com.cshelper.cshelper.entity.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Integer> {
+
+  // content id를 통해 검색, 결과값을 ContentDto로 반환
+  @Query("SELECT new com.cshelper.cshelper.dto.ContentDto(cat.name, c.title, c.content, c.level)" +
+          "FROM Content c JOIN Category cat ON c.categoryId = cat.id WHERE c.id = :contentId ")
+  Optional<ContentDto> findContentDtoById(@Param("contentId") Integer contentId);
 }
