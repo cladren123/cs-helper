5998beab1008e7a22ef5f738f086a80f93f7cffc
KimDongEon <gold3987@naver.com>
2024-01-18 01:23:05 +0900

    fix(CategoryMapper) : 등록dto - 카테고리엔티티 메소드 이름 수정

--- cshelper/src/main/java/com/cshelper/cshelper/mapper/CategoryMapper.java
+++ cshelper/src/main/java/com/cshelper/cshelper/mapper/CategoryMapper.java
@@ -6,7 +6,7 @@ import com.cshelper.cshelper.entity.Category;
 public class CategoryMapper {
 
   // Register Dto -> Category로 반환
-  public static Category RegisterDtoToCategory(CategoryRegisterDto dto) {
+  public static Category registerDtoToCategory(CategoryRegisterDto dto) {
     return Category.builder()
             .name(dto.getName())
             .description(dto.getDescription())
