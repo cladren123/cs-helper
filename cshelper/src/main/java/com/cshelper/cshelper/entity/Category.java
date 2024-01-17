556d185ce3659e1e1b767823738e21e7c1eaa5b6
KimDongEon <gold3987@naver.com>
2024-01-18 01:52:46 +0900

    feat(Category) : 카테고리 엔티티 생성

--- cshelper/src/main/java/com/cshelper/cshelper/entity/Category.java
+++ cshelper/src/main/java/com/cshelper/cshelper/entity/Category.java
@@ -1,2 +1,26 @@
-package com.cshelper.cshelper.entity;public class Category {
+package com.cshelper.cshelper.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "category")
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  @NotNull
+  private String name;
+  @NotNull
+  private String description;
+
 }
